467e9293ad37806fe60243d45342256504b65378
Deivid Marques <deividmarques@gmail.com>
2015-11-11 16:32:35 -0200

    starting drag and drop

--- source/assets/javascripts/_general.js
+++ source/assets/javascripts/_general.js
@@ -1,10 +1,76 @@
-var mediakit = mediakit || {};
-mediakit.general = (function() {
+var yata = yata || {};
+yata.general = (function() {
   'use strict';
 
   function init() {
     console.log('start JS');
+    initCookie();
+    dragDrop();
   }
+
+  function dragDrop(){
+    $(".recebeDrag").sortable({
+      connectWith: ['.recebeDrag'],
+      placeholder: 'dragHelper',
+      scroll: true,
+      revert: true,
+      stop: function( e, ui ) {
+        salvaCookie();
+      }
+    });
+  }
+
+
+
+
+      // // minimizar boxes
+      // $('.lnk-minimizar').click(function(){
+      //   var ul = $(this).parent().parent().parent().find('ul');
+      //   if( $(ul).is(':visible') ) {
+      //     $(ul).slideUp();
+      //     $(this).html('[ + ]');
+      //   } else {
+      //     $(ul).slideDown();
+      //     $(this).html('[ - ]');
+      //   }
+      //   return false;
+      // });
+      // // remover box
+      // $('.lnk-remover').click(function(){
+      //   $(this).parent().parent().parent().fadeOut();
+      //   return false;
+      // });
+
+
+
+     // configuração inicial do cookie
+
+    function initCookie(){
+      if( $.cookie('df_draganddrop') ) {
+        var ordem = $.cookie('df_draganddrop').split('|');
+        // posiciona boxes nos containers certos
+        $('.cs-list-pages .itemDrag').each(function(){
+          if( ordem[0].search( $(this).attr('id') ) == -1 ) $('#drop-direita').append($(this));
+        });
+        // $('#drop-direita div.itemDrag').each(function(){
+        //   if( ordem[1].search( $(this).attr('id') ) == -1 ) $('#drop-esquerda').append($(this));
+        // });
+        // ordena containers
+        var esquerda = ordem[0].split(',');
+        for( i = 0; i<= esquerda.length; i++ ) $('.cs-list-pages').append($('#'+esquerda[i]));
+        // var direita = ordem[1].split(',');
+        // for( i = 0; i<= direita.length; i++ ) $('#drop-direita').append($('#'+direita[i]));
+        } else {
+          $.cookie('df_draganddrop', '', { expires: 7, path: '/' });
+        }
+    }
+
+
+    // salva cookie
+    function salvaCookie() {
+      var ordem = $('.cs-list-pages').sortable('toArray');
+      $.cookie('df_draganddrop', ordem);
+    };
 
   return {
     init: init,
--- source/assets/javascripts/_initializer.js
+++ source/assets/javascripts/_initializer.js
@@ -1,52 +1,20 @@
-var mediakit = (function() {
+var yata = (function() {
   'use strict';
 
-  // Used to run scripts when the HTML is ready
-  function fastInit() {
-    console.log('fast Init')
-  }
-
-  // Used to run scripts that just when all things are ready
   function init() {
-    console.log('initializer functions')
-    loadModules();
-    mediakit.general.init();
+    yata.general.init();
   }
 
-  function loadModules() {
-    var modules = getModules();
-    for (var i in modules) {
-      mediakit[modules[i]].init();
-      console.info("MediaKit: module [" + modules[i] + "] successfully initialized.");
-    }
-  }
-
-  function getModules() {
-    var modules = [];
-    $("[data-mk-module]").each(function () {
-      var module = $(this).data("mk-module");
-      if ( modules.indexOf(module) === -1 ) {
-        modules.push(module);
-      }
-    });
-    return modules;
-  }
 
   return {
-    init: init,
-    fastInit: fastInit
+    init: init
   };
 
 }());
 
-var mk = mediakit;
-
-$(window).load(function() {
-  'use strict';
-  mk.init();
-});
+var mk = yata;
 
 $(document).ready(function(){
   'use strict';
-  mk.fastInit();
+  mk.init();
 });
