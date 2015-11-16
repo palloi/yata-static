89232ce2d7cbf366985e71144163d37196aba033
Deivid Marques <deividmarques@gmail.com>
2015-11-16 10:45:59 -0200

    create function to show only one popover when label add page or link is clicked

--- source/assets/javascripts/_general.js
+++ source/assets/javascripts/_general.js
@@ -3,6 +3,23 @@ yata.general = (function() {
   'use strict';
 
   function init() {
+    dragDropMenu();
+    toggleLabelPopover();
+  }
+
+
+  function toggleLabelPopover(){
+    $('#ember590 label').on('click', function(){
+      var target = $(this).attr('for');
+      if( target == 'cs-sidebar-new-page'){
+        $('#cs-sidebar-new-link').removeAttr('checked');
+      } else{
+        $('#cs-sidebar-new-page').removeAttr('checked');
+      }
+    });
+  }
+
+  function dragDropMenu(){
     $( '#sortable' ).sortable({
       cancel: '.ui-state-disabled',
       items: 'li:not(.ui-state-disabled)',
@@ -13,7 +30,7 @@ yata.general = (function() {
   }
 
   return {
-    init: init,
+    init: init
   };
 
 }());
