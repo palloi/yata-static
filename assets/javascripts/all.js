4644223272cc9fc8afa16e394b8835db9ad387ef
Deivid Marques <deividmarques@gmail.com>
2015-11-18 09:15:44 -0200

    Automated commit at 2015-11-18 11:15:44 UTC by middleman-deploy 1.0.0

--- assets/javascripts/all.js
+++ assets/javascripts/all.js
@@ -3,6 +3,27 @@ yata.general = (function() {
   'use strict';
 
   function init() {
+    dragDropMenu();
+    toggleLabelPopover();
+    linkPreventDefault();
+    createTitlePage();
+    confirmNewPage();
+    cancelNewPage();
+  }
+
+
+  function toggleLabelPopover(){
+    $('#ember590 label').on('click', function(){
+      var target = $(this).attr('for');
+      if( target == 'cs-sidebar-new-page'){
+        $('#cs-sidebar-new-link').removeAttr('checked');
+      } else{
+        $('#cs-sidebar-new-page').removeAttr('checked');
+      }
+    });
+  }
+
+  function dragDropMenu(){
     $( '#sortable' ).sortable({
       cancel: '.ui-state-disabled',
       items: 'li:not(.ui-state-disabled)',
@@ -12,8 +33,56 @@ yata.general = (function() {
     $( '#sortable li' ).disableSelection();
   }
 
+
+  function linkPreventDefault(){
+    $('a').on('click', function(e){
+      if($(this).attr('href') === '' || $(this).attr('href') === '#'){
+        e.preventDefault();
+      }
+    })
+  }
+
+  function createTitlePage(){
+    var $field = $('#title-page');
+
+    $field.on('keyup', function(){
+      $('#url-page').val($(this).val());
+    });
+
+  }
+
+  function clearFields(){
+    $('#url-page, #title-page').val("");
+  }
+
+  function loadingPage(){
+    $('.image-fake').addClass('hide');
+    $('.cssload-loader').addClass('show');
+    setTimeout(function(){
+      $('.cssload-loader').removeClass('show');
+      $('.image-empty').addClass('show');
+    },2000)
+  }
+
+
+
+
+  function confirmNewPage(){
+    $('#confirm-add-page').on('click', function(){
+      $('#add-page').trigger('click');
+      loadingPage();
+      clearFields();
+    });
+  }
+
+  function cancelNewPage(){
+    $('#cancel-add-page').on('click', function(){
+      clearFields();
+    });
+  }
+
   return {
-    init: init,
+    init: init
   };
 
 }());
